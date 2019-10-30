9b3c3f39e88df44350166cb163f5f3b15b2fcc09
MXW <3190212717@qq.com>
2019-10-30 16:46:34 +0800

    项目初始化2.0

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+//环境变量配置 dev / online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+
+// 抽离 html 模板插件
+var getHtmlConfig = function (name, title) {
+  return {
+    template: './src/view/' + name + '.html',
+    filename: 'view/' + name + '.html',
+    favicon: './favicon.ico',
+    title: title,
+    inject: true,
+    hash: true,
+    chunks: ['common', name]
+  }
+}
+
+var config = {
+  // 定义入口文件
+  entry: {
+    'common': ['./src/page/common/index.js'],
+    'index': ['./src/page/index/index.js'],
+  },
+  // 定义构建后文件的出口
+  output: {
+    path: __dirname + '/dist/',
+    publicPath: 'dev' === WEBPACK_ENV ? '/dist/' : '//s.happymmall.com/mmall-fe/dist/',
+    filename: 'js/[name].js'
+  },
+  // 插件
+  plugins: [
+    // 独立通用模块到 base.js
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/base.js'
+    }),
+    // 独立 css 文件
+    new ExtractTextPlugin("css/[name].css"),
+    // html 模板的处理
+    new htmlWebpackPlugin(getHtmlConfig('index')),
+  ],
+  // 关于模块的加载相关
+  module: {
+    loaders: [{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/,
+        loader: 'url-loader?limit=300&name=resource/[name].[ext]'
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      node_modules: __dirname + '/node_modules',
+      util: __dirname + '/src/util',
+      page: __dirname + '/src/page',
+      service: __dirname + '/src/service',
+      image: __dirname + '/src/image'
+    }
+  },
+  // 外部库或 api
+  extranls: {
+    'jquery': 'window.jQuery'
+  },
+};
+
+module.exports = config;
